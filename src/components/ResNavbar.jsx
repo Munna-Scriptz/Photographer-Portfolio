311636dd6bf5ecc843f835243b3b8d93fa107fea
Munna-Scripts <munnascriptz@gmail.com>
2025-08-11 12:31:27 +0600

    Navbar Responsived

--- src/components/ResNavbar.jsx
+++ src/components/ResNavbar.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/images/Logo.png'
 import { Link } from 'react-router'
 import { FaBars } from "react-icons/fa6";
-
-const Navbar = () => {
+const ResNavbar = () => {
+    const [navMenu = showNavMenu] = useState(false)
   return (
     <>
-        <nav className='absolute top-0 left-0 py-[29px] w-full lg:block hidden'>
+        <nav className='absolute top-0 left-0 py-[29px] px-[20px] w-full lg:hidden block'>
             <div className="container">
                 <div id="NavRow" className='flex items-center justify-between'>
-                    <div className='flex items-center gap-[129px]'>
                         <Link to={'/'}><img src={Logo} alt="Logo" /></Link>
-                        <ul className='flex items-center gap-[30px] text-white font-inter text-base opacity-[0.8]'>
+                        {/* <ul className='flex items-center gap-[30px] text-white font-inter text-base opacity-[0.8]'>
                             <li><Link to={"/"}>Home</Link></li>
                             <li><Link to={"/"}>About</Link></li>
                             <li><Link to={"/"}>Services</Link></li>
                             <li><Link to={"/"}>Contact</Link></li>
-                        </ul>
-                    </div>
+                        </ul> */}
                     <div>
-                        <FaBars className='text-white text-[32px] cursor-pointer'/>
+                        <FaBars onClick={()=>showNavMenu(!navMenu)} className='text-white text-[32px] cursor-pointer'/>
                     </div>
                 </div>
+            </div>
+            <div>
+
             </div>
         </nav>
     </>
   )
 }
 
-export default Navbar
+export default ResNavbar
